a2866b9dbe9c48e52fa75e8df2ddec9149472632
Andrew Mahone <andrew.mahone@gmail.com>
2010-08-21 22:38:37 -0400

    Fix user init mount checks, remove /etc/init.d from report of scripts run.

--- usr/initrd_files/user_early_init.sh
+++ usr/initrd_files/user_early_init.sh
@@ -2,21 +2,24 @@
 # Run user early init programs. These run before /data and /dbdata are mounted,
 # and may mount /data and /dbdata. If /data or /dbdata are not mount points
 # after early init completes, then they will be mounted using the standard
-# devices and filesystems. All operations are logged to /user.log.
+# devices and filesystems. All operations are logged to /system/user.log.
 export PATH=/sbin:/system/bin:/system/xbin
+mv /system/user.log /system/user.log.old
 exec >>/system/user.log
 exec 2>&1
 echo $(date) USER EARLY INIT START
-for file in /system/etc/init.d/E* ; do
-    if ! ls "$file" >/dev/null 2>&1 ; then continue ; fi
-    echo "START '$file'"
-    "$file"
-    echo "EXIT '$file' ($?)"
-done
+if cd /system/etc/init.d >/dev/null 2>&1 ; then
+    for file in E* ; do
+        if ! ls "$file" >/dev/null 2>&1 ; then continue ; fi
+        echo "START '$file'"
+        "./$file"
+        echo "EXIT '$file' ($?)"
+    done
+fi
 echo $(date) USER EARLY INIT DONE
 # Do this here instead of back in init.rc so that it can be conditional on the
 # target not already being a mount point.
-if ! mount -t remount remount /data >/dev/null 2>&1 ; then
+if ! mount -t none -o remount none /data >/dev/null 2>&1 ; then
     echo "MOUNT /data"
     if mount -t rfs -o noatime,nosuid,nodev,check=no /dev/block/mmcblk0p2 /data ; then
         echo "SUCCESS"
@@ -24,7 +27,7 @@ if ! mount -t remount remount /data >/dev/null 2>&1 ; then
         echo "FAILURE ($?)"
     fi
 fi
-if ! mount -t remount remount /dbdata >/dev/null 2>&1 ; then
+if ! mount -t none -o remount none /dbdata >/dev/null 2>&1 ; then
     echo "MOUNT /dbdata"
     if mount -t rfs -o noatime,nosuid,nodev,check=no /dev/block/stl10 /dbdata ; then
         echo "SUCCESS"
--- usr/initrd_files/user_init.sh
+++ usr/initrd_files/user_init.sh
@@ -1,17 +1,19 @@
 #!/system/bin/sh
 # Run user init programs. These are run just before class_default is started,
 # so they will have access to the complete environment. All actions are logged
-# to /user.log
+# to /system/user.log
 export PATH=/sbin:/system/bin:/system/xbin
 exec >>/system/user.log
 exec 2>&1
 echo $(date) USER INIT START
-for file in /system/etc/init.d/S* ; do
-    if ! ls "$file" >/dev/null 2>&1 ; then continue ; fi
-    echo "START '$file'"
-    "$file"
-    echo "EXIT '$file' ($?)"
-done
+if cd /system/etc/init.d >/dev/null 2>&1 ; then
+    for file in S* ; do
+        if ! ls "$file" >/dev/null 2>&1 ; then continue ; fi
+        echo "START '$file'"
+        "./$file"
+        echo "EXIT '$file' ($?)"
+    done
+fi
 echo $(date) USER INIT DONE
 # Allow init to proceed
 setprop user_init.init.completed 1
